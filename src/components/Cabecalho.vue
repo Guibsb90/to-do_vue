014728ac07a46bfa460c1fafd5120a082416e4f5
Guibsb90 <122750380+Guibsb90@users.noreply.github.com>
2024-07-27 12:37:37 -0300

    Finalizado

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps(['tarefasPendentes']);
+</script>
+
+<template>
+  <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+    <h1>Minhas Tarefas</h1>
+    <p>
+      Você possui {{ tarefasPendentes }} tarefas pendentes
+    </p>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  background-color: #f5f5f5;
+  padding: 10px;
+  text-align: center;
+}
+</style>
